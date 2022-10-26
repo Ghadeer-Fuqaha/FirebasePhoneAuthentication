021b208888e618604efb6e0393e95f52ac35bf4b
ghadeer fuqaha <ghadeer.alfuqaha@gmail.com>
2022-10-26 12:34:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebasephoneauthentication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.firebasephoneauthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout mobileNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mobileNumber = (TextInputLayout) findViewById(R.id.TextInputPhoneLayout);
+
+        findViewById(R.id.sendVer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String phone = mobileNumber.getEditText().getText().toString().trim();
+
+                //Validate phone number before send it to VerifyCodeActivity
+
+                if(phone.isEmpty() || phone.length()<10){
+
+                    mobileNumber.setError("Enter valid phone number:");
+                    mobileNumber.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(MainActivity.this,VerifyCodeActivity.class);
+                intent.putExtra("mobile",phone);
+                startActivity(intent);
+            }
+        });
+    }
+}
